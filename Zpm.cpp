c0f6b061be2eb81bd20e15bcada903fb9b321871
Landon Deam <landondeam@gmail.com>
2024-05-08 14:33:20 -0400

    some lexing stuff

--- Zpm.cpp
+++ Zpm.cpp
@@ -14,15 +14,30 @@ class Token {
     std::string TokenType;
     std::string TokenValue;
 
+private:
+    static const std::regex string("(['\"`])([^\1]*?)((?<!\\)\1)");
+    static const std::regex integer("-?[0-9]+");
+    static const std::regex variable("[a-zA-Z_][a-zA-Z_0-9]*(?=\s|;|\n)");
+    static const std::regex assign("(?<![<>])[+\-*\/]?=");
+    static const std::regex compare("(?<![+\-*\/])[<>=!]=");
+    static const std::regex end_statement(";");
+
+public:
     Token(std::string Type, std::string Value) {
         this->TokenType = Type;
         this->TokenValue = Value;
     }
 
-    static Token LexicalAnalysis(std::string str) {
-        return Token("","");
-        // string: (['"`])([^\1]*?)(\1)
-        // variable: [a-zA-Z_][a-zA-Z_0-9]+(?=\s)
+    static std::vector<Token> LexicalAnalysis(std::string str) {
+        std::vector<Token> tokens = std::vector<Token>();
+
+        return tokens;
+        // string: (['"`])([^\1]*?)((?<!\\)\1)
+        // integer: -?[0-9]+
+        // variable: [a-zA-Z_][a-zA-Z_0-9]*(?=\s|;|\n)
+        // assignment: (?<![<>])[+\-*\/]?=
+        // compare: (?<![+\-*\/])[<>=!]=
+        // end statement: ;
     }
 };
 
