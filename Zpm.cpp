ef4db74ce95b845393fae54dc99d54befe567174
Landon Deam <landondeam@gmail.com>
2024-05-08 16:18:26 -0400

    added some more stuff

--- Zpm.cpp
+++ Zpm.cpp
@@ -1,22 +1,18 @@
+// Copyright Landon Deam 2024
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <regex>
 
-int main(int argc, char** argv) {
-
-
-}
+std::string slurp(std::ifstream& in);
 
 class Token {
-
-public:
+ public:
     std::string TokenType;
     std::string TokenValue;
-    int TokenIndex;
 
-public:
     Token(std::string Type, std::string Value) {
         this->TokenType = Type;
         this->TokenValue = Value;
@@ -24,18 +20,21 @@ public:
 
     static std::vector<Token> LexicalAnalysis(std::string str) {
         std::vector<Token> tokens = std::vector<Token>();
-		static const std::regex string_reg("(['\"`])([^\1]*?)((?<!\\)\1)");
-    	static const std::regex integer("-?[0-9]+");
-    	static const std::regex variable("[a-zA-Z_][a-zA-Z_0-9]*(?=\\s|;|\n)");
-    	static const std::regex assign("(?<![<>])[+\\-*\\/]?=");
-    	static const std::regex compare("(?<![+\\-*\\/])[<>=!]=");
-    	static const std::regex end_statement(";");
-        
-        std::sregex_iterator str_begin =
-            std::sregex_iterator(str.begin(), str.end(), string_reg);
-        std::sregex_iterator str_end = std::sregex_iterator();
-        for (std::sregex_iterator i = str_begin; i != str_end; i++) {
-            
+        static const std::regex string_reg("(['\"`])" "([^\1]*?)""\1");
+        static const std::regex integer("-?[0-9]+");
+        static const std::regex variable("[a-zA-Z_][a-zA-Z_0-9]*(?=\\s|;|\n)");
+        static const std::regex assign("(?<![<>])[+\\-*\\/]?=");
+        static const std::regex compare("(?<![+\\-*\\/])[<>=!]=");
+        static const std::regex end_statement(";");
+
+        std::smatch str_result;
+        if (std::regex_match(str, str_result, string_reg)) {
+            std::smatch::iterator str_it = str_result.begin();
+            for (std::advance(str_it, 1);
+                 str_it != str_result.end();
+                 advance(str_it, 1)) {
+                std::cout << *str_it << std::endl;
+            }
         }
 
         return tokens;
@@ -48,3 +47,32 @@ public:
     }
 };
 
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Incorrect number of arguments (needs a file to run).\n";
+        return 0;
+    } else if (argc > 2) {
+        std::cout <<
+            "Incorrect number of arugments (only executes one file).\n";
+        return 0;
+    }
+    std::ifstream input;
+    try {
+        input = std::ifstream(argv[1]);
+    } catch (std::exception e) {
+        input.close();
+        std::cerr << e.what() << std::endl;
+        return 0;
+    }
+
+    Token::LexicalAnalysis(slurp(input));
+
+    input.close();
+    return 0;
+}
+
+std::string slurp(std::ifstream& in) {
+    std::ostringstream sstr;
+    sstr << in.rdbuf();
+    return sstr.str();
+}
